de6c3903a82e3028d0095b81e7dda1ab7c80da87
薛定谔的猫 <hh_2013@foxmail.com>
2016-12-09 04:45:17 +0800

    use buble, replace babel

---
+++ dist/underscore.es.js
@@ -0,0 +1,27 @@
+function __commonjs(fn, module) { return module = { exports: {} }, fn(module, module.exports), module.exports; }
+
+var underscore = __commonjs(function (module) {
+var _ = {};
+module.exports = _;
+
+// 常用的函数
+_.ø = function () {};
+_.identity = function ( x ) { return x; };
+_.toString = ({}).toString;
+
+// 类型判断
+_.isType = function ( type ) { return function ( val ) { return _.toString.call(val) === '[object ' + type + ']'; }; };
+
+_.isFunc = _.isType('Function');
+_.isArray = _.isType('Array');
+_.isNumber = _.isType('Number');
+_.isString = _.isType('String');
+_.isBoolean = _.isType('Boolean');
+_.isNull = _.isType('Null');
+_.isUndefined = _.isType('Undefined');
+});
+
+var underscore$1 = (underscore && typeof underscore === 'object' && 'default' in underscore ? underscore['default'] : underscore);
+
+export default underscore$1;
+//# sourceMappingURL=underscore.es.js.map
---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import buble from 'rollup-plugin-buble';
+import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+var external = process.env.DEPS ? [] : [ 'acorn/dist/acorn.js', 'magic-string' ];
+
+export default {
+    entry: 'underscore',
+    moduleName: 'underscore',
+    plugins: [
+        json(),
+        commonjs(),
+        buble({
+            include: [
+                'underscore.js',
+            ],
+            transforms: {
+                dangerousForOf: true
+            }
+        }),
+        nodeResolve({
+            jsnext: true,
+            skip: external
+        })
+    ],
+    external: external,
+    globals: {
+        'acorn/dist/acorn.js': 'acorn',
+        'magic-string': 'MagicString'
+    },
+    sourceMap: !process.env.DEPS
+};
